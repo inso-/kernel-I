490f185c49ac3236655528140f1f39f7d972ec03
Gregoire Lionnet <gregoirelionnet@gmail.com>
2015-10-26 22:15:38 +0100

    Finnaly understood what I was supposed to do, here is printk on video output.

--- kfs/src/main.c
+++ kfs/src/main.c
@@ -1,4 +1,16 @@
+void printk(const char *msg)
+{
+    volatile char *video = (volatile char*)0xB8500;
+
+    while (*msg != 0)
+    {
+        *video++ = *msg++;
+        *video++ = 42;
+    }
+}
+
 int main()
 {
-    for (;;);
+    printk("Hello World!");
+    return 0;
 }
